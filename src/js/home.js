6d52d2952c91c36c2684e192d9dc138b15539c15
Luis david <david12.29@hotmail.com>
2019-11-12 21:50:19 -0500

    problem

--- src/js/home.js
+++ src/js/home.js
@@ -77,34 +77,54 @@ fetch('https://randomuser.me/api/')
   
   const $featuringContainer=document.getElementById("featuring");
   const $home=document.getElementById("home");
-  const $form=document.querySelector("#form");
+  const $form=document.getElementById("form");
   
   function setAttributes($element, attributes) {
     for (const attribute in attributes) {
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
-  $form.addEventListener("submit",async(event)=>{
+
+  const BASE_API = 'https://yts.am/api/v2/';
+
+  function featuringTemplate(peli) {
+    return (
+      `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+      `
+    )
+  }
+
+  $form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    $home.classList.add("search-active");
-    const $loader=document.createElement("img");
-    setAttributes($loader,{
-      src:"src/images/loader.gif",
-      height:50,
-      width:50,
+    $home.classList.add('search-active')
+    const $loader = document.createElement('img');
+    setAttributes($loader, {
+      src: 'src/images/loader.gif',
+      height: 50,
+      width: 50,
     })
-    
     $featuringContainer.append($loader);
-    const data=new FormData($form);
-    const data=await getData(`${data.get("name")}`)
-    
+
+    const data = new FormData($form);
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    const HTMLString = featuringTemplate(peli.data.movies[0]);
+    $featuringContainer.innerHTML = HTMLString;
   })
-  
-  const actionList=await getData("https://yts.lt/api/v2/list_movies.json?genre=action")
-  const dramaList=await getData("https://yts.lt/api/v2/list_movies.json?genre=drama")
-  const animationList=await getData("https://yts.lt/api/v2/list_movies.json?genre=animation")
-  console.log(actionList,dramaList,animationList)
-  console.log("Action list",actionList);
+
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
+  console.log(actionList, dramaList, animationList)
+
   // const $home=$(".home");
   const $actionContainer=document.querySelector("#action");
   const $dramaContainer=document.getElementById("drama");
